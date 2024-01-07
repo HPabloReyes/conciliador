8f07cee9ea6fc1d44d73e5bdbda031cb264561c8
Pablo Reyes <chugo940@gmail.com>
2024-01-06 23:26:36 -0600

    first commit

---
+++ app/components/joke.jsx
@@ -0,0 +1,11 @@
+export default function Joke() {
+  return (
+    <div>
+      <p className="text-center text-2xl ">Ya vete a dormir</p>
+      <img
+        className="my-2"
+        src="https://www.publico.es/yo-animal/wp-content/uploads/2023/01/gatos-duermen-mucho-3-1024x683.jpg"
+      ></img>
+    </div>
+  );
+}
